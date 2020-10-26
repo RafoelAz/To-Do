3b8e9fb56931f4f4f1f6e1c27f8f85db2d37643d
rafoel <rafoelaz@gmail.com>
2020-10-26 17:29:49 +0300

    not adding task

--- main.js
+++ main.js
@@ -1,7 +1,51 @@
-	let inputToDo = document.querySelector('.input-to-do')
-        
+
+class ListToDoItem  {
+	constructor(textToDo){
+		this.textToDo = textToDo;
+	}
+	render() {
+		return`
+		<li class="list-to-do-item">
+		<span>${this.textToDo}</span>
+		<input class="checkbox" type="checkbox">
+		</li>`
+	}
+}
+
+class ListToDo {
+	constructor(){
+		this.toDoItem = []
+	}
+	addToDoItem(text){
+		this.toDoItem.push({textToDo: text})
+	}
+	fetchToDoItem(){
+		this.toDoItem = [
+			{textToDo: "Hallo World"},
+			{textToDo: "Hallo World"},
+			{textToDo: "Hallo World"},
+			{textToDo: "Hallo World"},
+		]
+	}
+	render(){
+		let toDoHtml = ""
+		this.toDoItem.forEach(({textToDo})=>{
+			const toDoItem = new ListToDoItem (textToDo)
+			toDoHtml += toDoItem.render()
+		})
+		document.querySelector('.list-to-do').innerHTML = toDoHtml
+	}
+	
+}
+
+const runToDo = new ListToDo()
+runToDo.fetchToDoItem()
+runToDo.render()
+
+let inputToDo = document.querySelector('.input-to-do')
     inputToDo.addEventListener('keydown', function (e) {
       if (e.keyCode === 13){
-          console.log(inputToDo.value)
+		runToDo.addToDoItem(inputToDo.value)
+		console.log(inputToDo.value)
       }
     });
